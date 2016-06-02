3fbbc4de078b7f7e4867b2662ce74640250f071e
Jayme-T <galvanize@Jamies-MacBook-Air-2.local>
2016-06-02 11:01:04 -0600

    made if so you can only delete items you created in the index.html file

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyB-Yv2W_XARpu5sx1l9BDXaQ2SNTmIB0tc",
@@ -12,33 +11,95 @@ firebase.initializeApp(config);
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    console.log("User Currently Signed in: ", user);
+    //console.log("User Currently Signed in: ", user);
     //route to posts
     runApp(user);
   } else {
-    console.log("No user signed in: ", user);
+    //console.log("No user signed in: ", user);
     //route to login page
     window.location.assign('/auth/login.html');
   }
 });
 
 function runApp(user) {
-    console.log("user: ", user);
+    //console.log("user: ", user);
     var firebaseDatabase = new FirebaseDatabase(user.uid);
 
 
     //main app code here
-    document.querySelector('#press').addEventListener('click', userClicksAButton);
 
-    function userClicksAButton() {
-      console.log('test');
 
-    	var inputtedData = {
-    		title: document.querySelector('#title'),
-    		body: document.querySelector('textarea')
-    	};
-    console.log(inputtedData);
-    	// Post.createNew(inputtedData);
-    }
 
-}
+        var button = document.getElementById('press');
+                button.addEventListener('click', function() {
+                    var title = document.getElementById("title");
+                    var body = document.querySelector('textarea');
+                    firebaseDatabase.createNewPost(title.value, body.value);
+                });
+                var postList = document.getElementById('posts-list');
+                console.log(postList);
+            function appendToList(item) {
+
+                if (item.isFinished === true) {return; }
+                var listitem = document.createElement('li');
+                    listitem.className = "list-item";
+                    listitem.data = item; //necessary! to hold data
+
+                    var rmv = document.createElement('i');
+                        rmv.className = "fa fa-times rmv";
+
+                        rmv.style.float = "left";
+                        rmv.addEventListener('click', removeFromList);
+
+                    var itemTitle = document.createElement('h3');
+                        itemTitle.className = 'item-text';
+                        itemTitle.innerHTML = item.title;
+                    var itemBody = document.createElement('p');
+                        itemBody.className = 'item-text';
+                        itemBody.innerHTML = item.body;
+                    var itemPID = document.createElement('p');
+                        itemPID.className = 'item-text';
+                        itemPID.innerHTML = item.pid;
+
+
+
+                listitem.appendChild(rmv);
+                listitem.appendChild(itemTitle);
+                listitem.appendChild(itemBody);
+                listitem.appendChild(itemPID);
+                postList.appendChild(listitem);
+
+                function removeFromList(e) {
+                  // console.log(e.target.parentElement.data.uid);
+                  // console.log(user.uid);
+                //   console.log(itemUID.innerHTML);
+                //   console.log(user);
+                  if(e.target.parentElement.data.uid !== user.uid){
+                    console.log("can't remove a post you didn't write");
+                  }
+                  else {
+
+              firebaseDatabase.deletePost(e.target.parentElement.data.pid);
+              // firebaseDatabase.setPostIsFinished(e.target.parentElement.data.pid);
+              e.target.parentElement.remove();
+            }
+
+          }
+
+
+            }
+
+            firebaseDatabase.onPostAdded(appendToList);
+
+
+                firebaseDatabase.onPostRemoved(function(data) {
+                    console.log("POST WAS REMOVED!: ", data);
+
+                });
+
+                firebaseDatabase.onPostUpdated(function(data) {
+                    console.log(data.uid);
+                    console.log("POST WAS edited!: ", data);
+                });
+
+    	}
