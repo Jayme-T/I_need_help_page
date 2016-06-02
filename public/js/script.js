87681f2cc1a656e1e477f2dec4c961b884651557
Jayme-T <galvanize@Jamies-MacBook-Air-2.local>
2016-06-02 13:05:36 -0600

    added edit and comment button and plan to go through and implement all event listeners soon

--- public/js/script.js
+++ public/js/script.js
@@ -2,23 +2,23 @@
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyB-Yv2W_XARpu5sx1l9BDXaQ2SNTmIB0tc",
-  authDomain: "helpwithx.firebaseapp.com",
-  databaseURL: "https://helpwithx.firebaseio.com",
-  storageBucket: "helpwithx.appspot.com",
+    apiKey: "AIzaSyB-Yv2W_XARpu5sx1l9BDXaQ2SNTmIB0tc",
+    authDomain: "helpwithx.firebaseapp.com",
+    databaseURL: "https://helpwithx.firebaseio.com",
+    storageBucket: "helpwithx.appspot.com",
 };
 firebase.initializeApp(config);
 
 firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    //console.log("User Currently Signed in: ", user);
-    //route to posts
-    runApp(user);
-  } else {
-    //console.log("No user signed in: ", user);
-    //route to login page
-    window.location.assign('/auth/login.html');
-  }
+    if (user) {
+        //console.log("User Currently Signed in: ", user);
+        //route to posts
+        runApp(user);
+    } else {
+        //console.log("No user signed in: ", user);
+        //route to login page
+        window.location.assign('/auth/login.html');
+    }
 });
 
 function runApp(user) {
@@ -30,85 +30,96 @@ function runApp(user) {
 
 
 
-        var button = document.getElementById('press');
-                button.addEventListener('click', function() {
-                    var title = document.getElementById("title");
-                    var body = document.querySelector('textarea');
-                    firebaseDatabase.createNewPost(title.value, body.value);
-                });
-                var postList = document.getElementById('posts-list');
-                console.log(postList);
-            function appendToList(item) {
+    var button = document.getElementById('press');
+    button.addEventListener('click', function() {
+        var title = document.getElementById("title");
+        var body = document.querySelector('textarea');
+        firebaseDatabase.createNewPost(title.value, body.value);
+    });
+    var postList = document.getElementById('posts-list');
+    console.log(postList);
 
-                if (item.isFinished === true) {return; }
-                var listitem = document.createElement('li');
-                    listitem.className = "list-item";
-                    listitem.data = item; //necessary! to hold data
+    function appendToList(item) {
 
-                    var rmv = document.createElement('i');
-                        rmv.className = "fa fa-times rmv";
+        if (item.isFinished === true) {
+            return;
+        }
+        var listitem = document.createElement('li');
+        listitem.className = "list-item";
+        listitem.data = item; //necessary! to hold data
 
-                        rmv.style.float = "left";
-                        rmv.addEventListener('click', removeFromList);
+        var rmv = document.createElement('i');
+        rmv.className = "fa fa-times rmv";
 
-                    var itemTitle = document.createElement('h3');
-                        itemTitle.className = 'item-text';
-                        itemTitle.innerHTML = item.title;
-                    var itemBody = document.createElement('p');
-                        itemBody.className = 'item-text';
-                        itemBody.innerHTML = item.body;
-                    var itemPID = document.createElement('p');
-                        itemPID.className = 'item-text';
-                        itemPID.innerHTML = item.pid;
+        rmv.style.float = "left";
+        rmv.addEventListener('click', removeFromList);
+
+        var itemTitle = document.createElement('h3');
+        itemTitle.className = 'item-text';
+        itemTitle.innerHTML = item.title;
+        var itemBody = document.createElement('p');
+        itemBody.className = 'item-text';
+        itemBody.innerHTML = item.body;
+        var itemPID = document.createElement('p');
+        itemPID.className = 'item-text';
+        itemPID.innerHTML = item.pid;
+
+        var editButton=document.createElement('button');
+        editButton.innerHTML="Edit";
+        editButton.style.marginLeft="40px";
+        // editButton.addEventListener('click', edit());
+        var CommentButton=document.createElement('button');
+        CommentButton.innerHTML="Comment";
+        CommentButton.style.marginLeft="40px";
+        // CommentButton.addEventListner('click', comment());
+
+        listitem.appendChild(rmv);
+        listitem.appendChild(itemTitle);
+        listitem.appendChild(editButton);
+        listitem.appendChild(CommentButton);
+
+        // listitem.appendChild(itemBody);
+        //I will add an event listenter and if you click on a topic
+        //you can see the whole questions and provide answers.
+        //That way long questions don't take the entire page.
+
+        //no need for people to see the PID
+        //listitem.appendChild(itemPID);
+        postList.appendChild(listitem);
+
+        function removeFromList(e) {
+            // console.log(e.target.parentElement.data.uid);
+            // console.log(user.uid);
+            //   console.log(itemUID.innerHTML);
+            //   console.log(user);
 
 
+            if (e.target.parentElement.data.uid !== user.uid) {
+                new Notification('error', "can't remove a question you didn't write", 3000)
 
-                listitem.appendChild(rmv);
-                listitem.appendChild(itemTitle);
-                //listitem.appendChild(itemBody);
-                //I will add an event listenter and if you click on a topic
-                //you can see the whole questions and provide answers.
-                //That way long questions don't take the entire page.
+            } else {
+                new Notification('success', 'question removed', 3000)
 
-                //no need for people to see the PID
-                //listitem.appendChild(itemPID);
-                postList.appendChild(listitem);
-
-                function removeFromList(e) {
-                  // console.log(e.target.parentElement.data.uid);
-                  // console.log(user.uid);
-                //   console.log(itemUID.innerHTML);
-                //   console.log(user);
-
-
-                  if(e.target.parentElement.data.uid !== user.uid){
-                    new Notification('error', "can't remove a question you didn't write", 3000)
-
-                  }
-                  else {
-                    new Notification('success', 'question removed', 3000)
-
-              firebaseDatabase.deletePost(e.target.parentElement.data.pid);
-              // firebaseDatabase.setPostIsFinished(e.target.parentElement.data.pid);
-              e.target.parentElement.remove();
+                firebaseDatabase.deletePost(e.target.parentElement.data.pid);
+                // firebaseDatabase.setPostIsFinished(e.target.parentElement.data.pid);
+                e.target.parentElement.remove();
             }
 
-          }
+        }
 
 
-            }
+    }
 
-            firebaseDatabase.onPostAdded(appendToList);
+    firebaseDatabase.onPostAdded(appendToList);
 
 
-                firebaseDatabase.onPostRemoved(function(data) {
-                    console.log("POST WAS REMOVED!: ", data);
+    firebaseDatabase.onPostRemoved(function(data) {
+        console.log("POST WAS REMOVED!: ", data);
 
-                });
+    });
 
-                firebaseDatabase.onPostUpdated(function(data) {
-                    console.log(data.uid);
-                    console.log("POST WAS edited!: ", data);
-                });
+    firebaseDatabase.onPostUpdated(function(data) {
+        console.log("POST WAS EDITED!: ", data);
 
-    	}
+    });
+}
